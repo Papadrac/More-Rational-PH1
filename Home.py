326131cb859267a4efbeeb6f514b3e47ed469406
Papadrac <lowietambis@gmail.com>
2026-02-08 14:31:28 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -106,7 +106,7 @@ st.markdown('''
 }
 </style>
 <div style="width:100%; display:flex; justify-content:center; gap:32px; margin-top:40px;">
-  <span class="circle-border"><a href="/Pages/0_About Us.py" class="rect-link yellow">About Us</a></span>
+  <span class="circle-border"><a href="https://more-rational-ph1-jqfq2eed47wdgf6c79n2as.streamlit.app/About_Us" class="rect-link yellow">About Us</a></span>
     <span class="circle-border"><a href="/Pages/0_BakitRationality.py" class="rect-link yellow">Bakit Rationality</a></span>
   <span class="circle-border"><a href="/Pages/1_Panuntunan.py" class="rect-link blue">Improve Your Thinking</a></span>
 </div>
@@ -126,4 +126,5 @@ st.markdown('''
   </div>
 </div>
 ''', unsafe_allow_html=True)
+
 
