361e4535ff312ddb55283fb051bfc1fbbd68cdf0
Papadrac <lowietambis@gmail.com>
2026-06-12 22:05:52 +0800

    Update 43_Lohika-Conditional Truth Values.py

--- pages/43_Lohika-Conditional Truth Values.py
+++ pages/43_Lohika-Conditional Truth Values.py
@@ -227,7 +227,7 @@ if st.session_state.submitted_last:
             st.rerun()
     with col_right:
         st.markdown(
-            "<a href='/Pages/44_Lohika-Congratulations' target='_self'><button style='font-size:16px;padding:8px 16px;border-radius:6px;background:#0099f6;color:white;border:none;cursor:pointer;'>Pumunta sa Susunod na Seksyon →</button></a>",
+            "<a href='https://morerationalph.streamlit.app/Congratulations' target='_self'><button style='font-size:16px;padding:8px 16px;border-radius:6px;background:#0099f6;color:white;border:none;cursor:pointer;'>Pumunta sa Susunod na Seksyon →</button></a>",
             unsafe_allow_html=True,
         )
 
